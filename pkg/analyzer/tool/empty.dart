a49f3c2356b81519705a7fce588b7d13d7efe02d
Bob Nystrom <rnystrom@google.com>
2017-01-19 14:43:58 -0800

    Slap together script to generate report of SDK errors.
    Checking in the report too, so I can track how changes affect the
    results.

---
+++ pkg/analyzer/tool/empty.dart
@@ -0,0 +1,24 @@
+// A pointless empty library to give analyzer something to analyze when we
+// really want it to also look at the SDK itself. See generate_sdk_errors.dart.
+import 'dart:async';
+import 'dart:core';
+
+// Would be good to look at some of these.
+import 'dart:collection';
+import 'dart:convert';
+import 'dart:html';
+import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+
+// Probably don't need to worry about these for the prototype.
+//import 'dart:developer';
+//import 'dart:indexed_db';
+//import 'dart:isolate';
+//import 'dart:js';
+//import 'dart:js_util';
+//import 'dart:mirrors';
+//import 'dart:svg';
+//import 'dart:web_audio';
+//import 'dart:web_gl';
+//import 'dart:web_sql';
